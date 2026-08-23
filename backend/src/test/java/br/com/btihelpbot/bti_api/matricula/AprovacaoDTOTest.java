fb2db0477d5bf0fe90687908f5ae4ad5cd26344d
jcassio-dev <josecassio2013@gmail.com>
2026-08-23 09:51:41 -0300

    expoe o slug do professor na resposta da api

--- backend/src/test/java/br/com/btihelpbot/bti_api/matricula/AprovacaoDTOTest.java
+++ backend/src/test/java/br/com/btihelpbot/bti_api/matricula/AprovacaoDTOTest.java
@@ -11,10 +11,12 @@ class AprovacaoDTOTest {
         TaxaAprovacao t = new TaxaAprovacao();
         t.setComponenteCodigo("MAT0031");
         t.setDocenteNome("PROF A");
+        t.setDocenteSlug("prof-a");
         t.setDesfechos(new Desfechos(38, 4, 0, 10));
 
         AprovacaoDTO dto = AprovacaoDTO.from(t);
 
+        assertEquals("prof-a", dto.docenteSlug());
         assertEquals(38, dto.aprovados());
         assertEquals(4, dto.reprovadosNota());
         assertEquals(0, dto.reprovadosFalta());
